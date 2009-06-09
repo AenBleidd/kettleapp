c00b4c4c134412da425f2e4cf1dcd9106b051488
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2009-06-09 08:42:31 +0300

    Add existing files into the new project

---
+++ Executable/Kettle/Resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Kettle.rc
+//
+
+#define IDS_APP_TITLE			103
+#define ID_MANIFEST				1
+
+#define IDR_MAINFRAME			128
+#define IDD_KETTLE_DIALOG		102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_KETTLE				107
+#define IDI_SMALL				108
+#define IDC_KETTLE				109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#define ID_SOUND				130
+#define IDB_GO_BUTTON			140
+#define IDB_EXIT_BUTTON			141
+#define IDE_EDIT				142
+#define IDT_TIMER				143
+#define IDSITE					144
+
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
---
+++ Flash/Kettle/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+OPTS=-use-network=false
+
+/home/lestat/Documents/Programs/flex_sdk_3/bin/mxmlc $OPTS kettle.mxml
